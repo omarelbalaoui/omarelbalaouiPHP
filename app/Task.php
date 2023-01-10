1d7c7366af536f02df94ba26db49bc7f8e5ccfc5
omarelbalaoui <omarelbalaoui@iesebre.com>
2023-01-10 21:25:05 +0100

    101 Casi acabat

---
+++ app/Task.php
@@ -0,0 +1,16 @@
+<?php
+class Task {
+    protected $id;
+    protected $title;
+    protected $description;
+    protected $completed;
+
+    public function __construct($id,$title,$description,$completed)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->completed = $completed;
+    }
+
+}
--- app/index.php
+++ app/index.php
@@ -1,2 +1,30 @@
 <?php
-$greeting = 'Hola mon!';
+require 'app/helpers.php';
+require 'app/Task.php';
+
+//$Task = new Task (1,'comprar pa','a la panaderia',1);
+//var_dump($Task);
+
+$user = 'debian-sys-maint';
+$pass = '51koikcsqa3MybPo';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=phplaravel',$user,$pass);
+}catch (\Exception $e){
+    echo 'Error de connexió';
+}
+
+$statment = $dbh->prepare('SELECT * FROM tasks;');
+
+$statment->execute();
+
+$tasks = $statment->fetchAll(PDO::FETCH_CLASS,'Task');
+var_dump($tasks);
+
+//QUERY STRING
+//var_dump($_GET['name']);
+//$name = 'Omar';
+//API
+
+$greeting = greet();
+
